aa5331606d7b0b7fd8f3ad443343d0f809f1a263
xiexindong <779788582@qq.com>
2019-11-07 18:38:42 +0800

    1

--- server/server.js
+++ server/server.js
@@ -8,6 +8,9 @@ const http = require("http").Server(app)
 const io = require("socket.io")(http)
 
 io.on('connection', function(socket){
+    socket.on('sendmsg', function(msg){
+        console.log('message: ' + msg);
+    });
     console.log('a user connected');
 });
 
